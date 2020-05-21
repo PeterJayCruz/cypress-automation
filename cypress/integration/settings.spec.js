1fd949f5b1b37f4c6d4c42d6e43e4bbef00c1913
PeterJayCruz <pcruz1@mail.sfsu.edu>
2020-05-20 17:26:21 -0700

    added settings page test cases - update profile pic, update bio, update username

---
+++ cypress/integration/settings.spec.js
@@ -0,0 +1,85 @@
+context('Settings Page', () => {
+  const testUsername = 'username_'.concat(new Date().valueOf());
+  const testEmail = testUsername.concat('@email.com');
+  const testPassword = 'test'
+
+  context('Update User Profile Information', () => {
+    before(() => {
+      cy.createNewUser({
+          username: testUsername,
+          email: testEmail,
+          password: testPassword
+        });
+    });
+
+    beforeEach(() => {
+      cy.login({
+        email: testEmail,
+        password: testPassword
+      })
+      .visit('/settings');
+    });
+
+    it('updates the profile picture', () => {
+      const testImageUrl = 'https://st3.depositphotos.com/3369547/14515/v/1600/depositphotos_145158759-stock-illustration-men-faceless-profile.jpg';
+
+      cy.get('input[placeholder="URL of profile picture"]')
+        .clear()
+        .type(testImageUrl)
+        .get('button')
+        .contains('Update Settings')
+        .click()
+
+      cy.url()
+        .should('eq', `${Cypress.config().baseUrl}/`)
+        .get('.user-pic')
+        .as('user image')
+        .should('have.prop', 'src')
+        .and('eq', testImageUrl);
+    });
+
+    it('updates the bio', () => {
+      cy.get('textarea[placeholder="Short bio about you"]')
+        .clear()
+        .type('this is a short bio about me')
+        .get('button')
+        .contains('Update Settings')
+        .click()
+        .url()
+        .should('eq', `${Cypress.config().baseUrl}/`);
+    });
+
+  });
+
+  context('Update User Login Information', () => {
+
+    beforeEach(() => {
+      const uniqueUsername = 'username_'.concat(new Date().valueOf());
+      const uniqueEmail = uniqueUsername.concat('@email.com');
+
+      cy.createNewUser({
+          username: uniqueUsername,
+          email: uniqueEmail,
+          password: testPassword
+        })
+        .login({
+          email: uniqueEmail,
+          password: testPassword
+        })
+        .visit('/settings');
+    });
+
+    it('updates the username', () => {
+      const anotherUniqueUsername = 'username_'.concat(new Date().valueOf());
+
+      cy.get('input[placeholder="Username"]')
+        .clear()
+        .type(anotherUniqueUsername)
+        .get('button')
+        .contains('Update Settings')
+        .click()
+        .url()
+        .should('eq', `${Cypress.config().baseUrl}/`);
+    });
+  });
+});
