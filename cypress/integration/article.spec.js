124ddea4602c5084444c53c116bba4d04454176c
PeterJayCruz <pcruz1@mail.sfsu.edu>
2020-05-29 20:17:42 -0700

    article page - added functions for adding and getting all comments

--- cypress/integration/article.spec.js
+++ cypress/integration/article.spec.js
@@ -1,5 +1,70 @@
 context('Article Page', () => {
 
+  const addComment = (token, commentOptions = {}) => {
+    const defaultCommentBody = 'comment_'.concat(new Date().valueOf());
+
+    const comment = Object.assign({
+      body: defaultCommentBody
+    }, commentOptions);
+
+    cy.request({
+      log: false,
+      method: 'POST',
+      url: `${Cypress.env('backEndBaseUrl')}/articles/${comment.articleTitle}/comments`,
+      headers: {
+        Authorization: token
+      },
+      body: {
+        comment: {
+          body: comment.body
+        }
+      }
+    })
+    .then((commentsResponse) => {
+      const log = Cypress.log({
+        name: 'addComment',
+        displayName: 'add comment',
+        message: comment.body,
+        consoleProps: () => {
+          return {
+            'Comments API response': commentsResponse,
+            'Article (input)': comment.articleTitle,
+            'Comment (input)': comment.body
+          };
+        }
+      });
+    });
+  };
+
+  const getComments = (apiPath) => {
+    return new Promise((resolve) => {
+      cy.request({
+        log: false,
+        method: 'GET',
+        url: `${Cypress.env('backEndBaseUrl')}/articles/${apiPath}/comments`,
+      })
+      .its('body.comments', {
+        log: false
+      })
+      .then((commentsResponse) => {
+        const log = Cypress.log({
+          name: 'getComments',
+          displayName: 'get comments',
+          message: apiPath,
+          consoleProps: () => {
+            return {
+              'Comments API response': commentsResponse,
+              'Article (input)': apiPath
+              // 'Comment (input)': comment.body
+            };
+          }
+        });
+
+        resolve(commentsResponse);
+      });
+    });
+  };
+
   context('positive', () => {
     context ('logged in as author', () => {
       let authHeaderToken = 'Token ';
@@ -80,26 +145,41 @@ context('Article Page', () => {
       let authHeaderToken = 'Token ';
       let article = {};
       let user = {};
+      let articleTitlePath = '';
 
       before(() => {
         cy.createUser({})
-          .its('body.user')
+          .its('body.user', {
+            log:false
+          })
           .then((userResponse) => {
             user = userResponse;
+            authHeaderToken = authHeaderToken.concat(userResponse.token);
+
             cy.createArticle({
               description: 'description_'.concat(new Date().valueOf()),
               body: 'body_'.concat(new Date().valueOf()),
               tagList: []
-            }, authHeaderToken.concat(userResponse.token))
-            .its('body.article')
+            }, authHeaderToken)
+            .its('body.article', {
+              log: false
+            })
             .then((articleResponse) => {
               article = articleResponse;
+              article.apiPath = articleResponse.title.toLowerCase().replace(/ /g, '-');
+
+              for(let i = 0; i < 5; i += 1) {
+                addComment(authHeaderToken, {
+                  articleTitle: article.apiPath,
+                  body: 'comment_'.concat((i+1).toString())
+                });
+              }
             });
           });
       });
 
       beforeEach(() => {
-        cy.visit(`/article/${article.title.toLowerCase().replace(/ /g, '-')}`);
+        cy.visit(`/article/${article.apiPath}`);
       });
 
       it('displays the article title', () => {
